917a12e29553a912d03da07ea5f71bb402b9836a
Leonardo de Bortoli <theleobortoli@gmail.com>
2024-11-08 00:43:15 -0300

    desenvolvimento 1

--- projeto.py
+++ projeto.py
@@ -1,7 +1,30 @@
 import random
 import numpy as np
 from typing import NewType
-class STAT():
+def val_en(self, enAtual):
+    if enAtual > self.enMax:
+        return self.enMax
+    elif enAtual < self.enMin:
+        return self.enMin
+    else:
+        return enAtual
+def val_hp(self, vidAtual):
+    if (self.vidAtual > self.vidMax):
+        return self.vidMax
+    if (self.vidAtual < self.vidMin):
+        return self.vidMin
+    else:
+        return vidAtual
+def rolarDado():
+    dadoTipo= input("O dado possui algum multiplicador? (S/N) ")
+    lados= int(input("Dado de quantos lados? "))
+    match dadoTipo:
+        case 'S':
+            multiplicador = int(input("Qual é o valor do multiplicador? "))
+            return print(f'O número rolado é {random.randint(1, lados) + multiplicador}')
+        case 'N':
+            return print("O número rolado é", random.randint(1, lados))
+class Stats():
     def __init__(self, idStat, vida: int, forca:int, destreza: int, tecnica: int, raciocinio: int):
         self.idStat = idStat
         self.vida = vida
@@ -35,7 +58,7 @@ class STAT():
         return self.raciocinio
     def __str__(self):
         return f'VIDA: {self.vida}, FORÇA: {self.forca}, DESTREZA: {self.destreza}, TÉCNICA: {self.tecnica}, RACIOCÍNIO: {self.raciocinio}'
-class PROF():
+class Profs():
     def __init__(self, idProf, mecanismo: int, externo: int, interno: int, exotico: int):
         self.idProf = idProf
         self.mecanismo = mecanismo
@@ -60,12 +83,15 @@ class PROF():
         return self.exotico
     def __str__(self):
         return f'MEC: {self.mecanismo}, EXT: {self.externo}, INT: {self.interno}, EXO: {self.exotico}'
-class HABILIDADE():
-    def __init__(self, nomHab: str, lvlHab: int, descHab: str, effHab):
+class Habilidades():
+    def __init__(self, codHab: int ,nomHab: str, lvlHab: int, descHab: str, effHab):
+        self.codHab = codHab
         self.nomHab = nomHab
         self.lvlHab = lvlHab
         self.descHab = descHab
         self.effHab = effHab
+    def setCodigoHab(self, codHab):
+        self.codHab = codHab
     def setNomeHab(self, nomHab):
         self.nomHab = nomHab
     def setLvlHab(self, lvlHab):
@@ -74,6 +100,8 @@ class HABILIDADE():
         self.descHab = descHab
     def setEffHab(self, effHab):
         self.effHab = effHab
+    def getCodigoHab(self):
+        return self.codHab
     def getNomeHab(self):
         return self.nomHab
     def getLvlHab(self):
@@ -82,11 +110,16 @@ class HABILIDADE():
         return self.descHab
     def getEffHab(self):
         return self.effHab
-class VIDAS(STAT):
-    def __init__(self, idStat, vida, vidAtual: int, vidTemp: int, vidMin: int):
-        super().__init__(self, idStat, vida)
+    def __str__(self):
+        return f'COD: {self.codHab} [HABILIDADE: {self.nomHab}] (NÍVEL: {self.lvlHab}): {self.descHab}. Seu efeito é {self.effHab}'
+class Capacidade():
+    def __init__(self, habilidade):
+        self.skills = habilidade
+class Vidas(Stats):
+    def __init__(self, idStat, vida, forca, destreza, tecnica, raciocinio, vidAtual: int, vidTemp: int, vidMin: int):
+        super().__init__(idStat, vida, forca, destreza, tecnica, raciocinio)
         self.vidMax = vida
-        self.vidAtual = vidAtual
+        self.vidAtual = val_hp(self, vidAtual)
         self.vidTemp = vidTemp
         self.vidMin = vidMin
     def setVidMax(self, vida):
@@ -94,11 +127,7 @@ class VIDAS(STAT):
             return False
         self.vidMax = vida
     def setVidAtual(self, vidAtual):
-        if (self.vidAtual > self.vidMax):
-            self.vidAtual = self.vidMax
-        if (self.vidAtual < self.vidMin):
-            self.vidAtual = self.vidMin
-        self.vidAtual = vidAtual
+        self.vidAtual = val_hp(self, vidAtual)
     def setVidTemp(self, vidTemp):
         self.vidTemp = vidTemp
     def setVidMin(self):
@@ -113,28 +142,32 @@ class VIDAS(STAT):
         return self.vidMin
     def __str___(self):
         return f'HP: {self.vidAtual} / {self.vidMax} TEMPORÁRIO: {self.vidTemp}'
-class ENERGIAS(STAT): 
-    def __init__(self, idStat, vida, forca, destreza, tecnica, raciocinio, statPrimario, statSecundario, enAtual: int, enMin: int):
+class Energias(Stats): 
+    def __init__(self, idStat, vida, forca, destreza, tecnica, raciocinio, statPrimario, statSecundario, enAtual: int, enMin: int, enMax = None):
         super().__init__(self, idStat, vida, forca, destreza, tecnica, raciocinio)
         self.stat1 = statPrimario
         self.stat2 = statSecundario
         self.mult1 = 1.0
-        self.mult2 = 0.5
-        self.enMax = int(int(self.stat1 * self.mult1) + int(self.stat2 * self.mult2))
-        self.enAtual = enAtual
-        self.enMin = enMin
+        self.mult2 = 0.5        
+        if self.enMax is not None:
+            self.enMax = enMax
+        else:
+            self.enMax = int(int(self.stat1 * self.mult1) + int(self.stat2 * self.mult2))
+        self.enAtual = val_en(self, enAtual)
+        if self.enMin < 0:
+            self.enMin = enMin
+        else:
+            self.enMin = 0
     def setStat1(self, statPrimario):
         self.stat1 = statPrimario
     def setStat2(self, statSecundario):
         self.stat2 = statSecundario
     def setEnAtual(self, enAtual):
-        if (self.enAtual > self.enMax):
-            self.enAtual = self.enMax
-        if (self.enAtual < self.enMin):
-            self.enAtual = self.enMin
-        self.enAtual = enAtual
-    def setEnMin(self, enMin):
-        self.enMin = 0
+        self.enAtual = val_en(self, enAtual)
+    def setEnMin(self, enMin): 
+        self.enMin = enMin
+    def setEnMax(self, enMax):
+        self.enMax = enMax
     def getStat1(self):
         return self.stat1
     def getStat2(self):
@@ -155,32 +188,86 @@ class ENERGIAS(STAT):
         return self.mult1
     def getMult2(self):
         return self.mult2
+    def setEnMin(self):
+        return self.enMin
+    def setEnMax(self):
+        return self.enMax
     def __str__(self):
         return f'ENERGIA: {self.enAtual} / {self.enMax}'
-class PERSONAGEM():
-    def __init__(self, persoNome: str, persoNivel, persoRaca: str, persoClasse, persoVidas, persoEnergia, persoStats, persoProfs):
+class Personagens():
+    def __init__(self, persoNome: str, persoNivel, persoRaca: str, persoClasse, persoVida, persoEnergia, persoStats, persoProfs, persoSkills):
         self.persoNome = persoNome
         self.persoNivel = persoNivel
         self.persoRaca = persoRaca
         self.classe = persoClasse
-        self.hp = persoVidas
+        self.hp = persoVida
         self.ep = persoEnergia
         self.stats = persoStats
         self.profs = persoProfs
+        self.habilidades = persoSkills
+    def setNome(self, persoNome):
+        self.persoNome = persoNome
+    def setNivel(self, persoNivel):
+        self.persoNivel = persoNivel
+    def setRaca(self, persoRaca):
+        self.persoRaca = persoRaca
+    def setClasses(self, persoClasse):
+        self.classe = persoClasse
+    def setHP(self, persoVida):
+        self.hp = persoVida
+    def setEP(self, persoEnergia):
+        self.ep = persoEnergia
     def setStats(self, persoStats):
         self.stats = persoStats
     def setProfs(self, persoProfs):
         self.profs = persoProfs
+    def setSkills(self, persoSkills):
+        self.habilidades = persoSkills
+    def getNome(self):
+        return self.persoNome
+    def getNivel(self):
+        return self.persoNivel
+    def getRaca(self):
+        return self.persoRaca
+    def getClasses(self):
+        return self.classe
+    def getHP(self):
+        return self.hp
+    def getEP(self):
+        return self.ep
     def getStats(self):
         return self.stats
     def getProfs(self):
         return self.profs
-def rolarDado():
-    dadoTipo= input("O dado possui algum multiplicador? (S/N) ")
-    lados= int(input("Dado de quantos lados? "))
-    match dadoTipo:
-        case 'S':
-            multiplicador = int(input("Qual é o valor do multiplicador? "))
-            return print(f'O número rolado é {random.randint(1, lados) + multiplicador}')
-        case 'N':
-            return print("O número rolado é", random.randint(1, lados)) 
+    def getSkills(self):
+        return self.habilidades
+    def __str__(self):
+        return f'NOME: {self.persoNome} NÍVEL: {self.persoNome} \
+        RAÇA: {self.persoRaca} CLASSE: {self.persoClasse} \
+        HP: {self.vidAtual} / {self.vidMax} TEMPORÁRIO: {self.vidTemp} + ENERGIA: {self.enAtual} / {self.enMax} \
+        STATS: {self.stats} \
+        PROFICIÊNCIAS: {self.profs} \
+        HABILIDADES: {self.habilidades}'
+
+listaStats = []
+listaProfs = []
+listaHabs = []
+listaCapc = []
+listaHPs = []
+listaEPs = []
+listaPesonagens = []
+continuar = int(1)
+
+while continuar >0:
+    print("1 - Cadastrar Stats")
+    print("2 - Cadastrar Proficiências")
+    print("3 - Cadastrar Habilidades")
+    print("4 - Cadastrar Capacidades")
+    print("5 - Cadastrar")
+    print("6 - Cadastrar")
+    print("7 - Cadastrar Personagem")
+    print("8 - Listar Fichas de Stats")
+    print("9 - Listar Fichas de Proficiências")
+    print("10 - Listar Habilidades")
+    print("11 - Listar Fichas de Capacidades")
+    print("12 - Listar Personagens")
